3988df1f690c6a12889d7bcf64f3a62627f90253
Franklin Ospina <fospima@gmail.com>
2024-07-17 16:32:27 -0500

    Se agrega primer trabajo

---
+++ dosPreguntas.py
@@ -0,0 +1,25 @@
+'''
+Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que 
+requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras 
+combinadas, para formar una marca creativa. 
+'''
+'''
+LA RATA (1936, 1948, 1960, 1972, 1984, 1996, 2008)
+EL BUEY (1937, 1949, 1961, 1973, 1985, 1997, 2009)
+EL TIGRE (1938, 1950, 1962, 1974, 1986, 1998, 2010)
+EL CONEJO (1939, 1951, 1963, 1975, 1987, 1999, 2011)
+EL DRAGÓN (1940, 1952, 1964, 1976, 1988, 2000, 2012)
+LA SERPIENTE (1941, 1953, 1965, 1977, 1989, 2001, 2013)
+EL CABALLO (1942, 1954, 1966, 1978, 1990, 2002, 2014)
+LA CABRA (1943, 1955, 1967, 1979, 1991, 2003, 2015)
+EL MONO (1944, 1956, 1968, 1980, 1992, 2004, 2016)
+EL GALLO (1945, 1957, 1969, 1981, 1993, 2005, 2017)
+EL PERRO (1946, 1958, 1970, 1982, 1994, 2006, 2018)
+EL CERDO (1947, 1959, 1971, 1983 1995, 2007, 2019)
+'''
+print("***************************************************\n")
+print("             !Vamos a crear tu marca¡                \n")
+print("***************************************************\n")
+pregunta_uno = input("Ingresa las primeras tres letras de tu nombre y las tres ultimas de tu primer apellido ")
+pregunta_dos = input("Sabes cual es tu animal del zodiaco chino? Ingresalo aca ")
+print("!Y el nombre de tu marca es¡\n"  + "\""+ pregunta_uno + " " + pregunta_dos +"\"")
